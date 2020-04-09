ba617a7308cc298f07b9f1e77a40d7a15733d5c7
Patrick Amrein <amrein@ubique.ch>
2020-04-09 14:25:43 +0200

    add documentation on models and update poms

--- star-sdk-backend/starsdk-model/src/main/java/ch/ubique/starsdk/model/ExposedOverview.java
+++ star-sdk-backend/starsdk-model/src/main/java/ch/ubique/starsdk/model/ExposedOverview.java
@@ -3,7 +3,10 @@ package ch.ubique.starsdk.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import ch.ubique.openapi.docannotations.Documentation;
+
 public class ExposedOverview {
+	@Documentation(description = "A list of all SecretKeys")
 	List<Exposee> exposed = new ArrayList<>();
 
 	public ExposedOverview() {
--- star-sdk-backend/starsdk-model/src/main/java/ch/ubique/starsdk/model/Exposee.java
+++ star-sdk-backend/starsdk-model/src/main/java/ch/ubique/starsdk/model/Exposee.java
@@ -4,11 +4,14 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import ch.ubique.openapi.docannotations.Documentation;
+
 public class Exposee {
 
 	private Integer Id;
 
 	@NotNull
+	@Documentation(description = "The SecretKey of a exposed.", example = "AAAA")
 	private String key;
 
 	public String getKey() {
