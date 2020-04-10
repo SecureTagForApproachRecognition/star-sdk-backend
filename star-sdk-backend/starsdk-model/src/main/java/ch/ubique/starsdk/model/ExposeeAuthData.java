002e720ec0d3a0b56b4fd460f215a243cbffdba1
Patrick Amrein <amrein@ubique.ch>
2020-04-10 11:52:32 +0200

    update documentation to include encoding for authdata

--- star-sdk-backend/starsdk-model/src/main/java/ch/ubique/starsdk/model/ExposeeAuthData.java
+++ star-sdk-backend/starsdk-model/src/main/java/ch/ubique/starsdk/model/ExposeeAuthData.java
@@ -4,7 +4,7 @@ import ch.ubique.openapi.docannotations.Documentation;
 
 public class ExposeeAuthData {
 
-	@Documentation(description =  "Authentication data used to verify the test result", example = "TBD")
+	@Documentation(description =  "Authentication data used to verify the test result (base64 encoded)", example = "TBD")
 	private String value;
 
 	public String getValue() {
